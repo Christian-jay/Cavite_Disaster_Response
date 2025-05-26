d0265313925b2c207228c771ca4713ad0cb50d5e
Christian-jay <lubochristianjay7@gmail.com>
2025-05-26 14:31:09 +0800

    Hell yeah

--- Cavite Disaster Response/script.js
+++ Cavite Disaster Response/script.js
@@ -1,4 +1,27 @@
- function myFunction() {
+const modal = document.getElementById("imgModal");
+const modalImg = document.getElementById("modalImage");
+const captionText = document.getElementById("caption");
+const closeBtn = document.querySelector(".close");
+
+document.querySelectorAll(".modal-image").forEach(img => {
+  img.addEventListener("click", function () {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  });
+});
+
+closeBtn.addEventListener("click", function () {
+  modal.style.display = "none";
+});
+
+window.addEventListener("click", function (event) {
+  if (event.target === modal) {
+    modal.style.display = "none";
+  }
+});
+
+function myFunction() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
         x.className += " responsive";
